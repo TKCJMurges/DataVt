84b19ed4382ad59ced1504fe7f35be724d487cfb
TKCJMurges <45498434+TKCJMurges@users.noreply.github.com>
2023-11-26 16:33:23 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -674,4 +674,4 @@ def update_bolinger_chart(selected_company, selected_timeframe):
 
 # Run the app
 if __name__ == '__main__':
-  app.run_server(debug=True)
+  app.run_server(debug=True, port=int(os.environ.get('PORT', 8050)))
